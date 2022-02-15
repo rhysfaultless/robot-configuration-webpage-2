c74ff7ad411e28e04f8bc5d1f03072db6837a1d1
Rhys Faultless <rhysfaultless@gmail.com>
2022-02-14 19:41:22 -0500

    Added price and lead time functions inside dingo-omni.js. I moved the data from several from several JSON files into a single file, so prices and lead times can be maintained on a per-robot basis.

--- components/three-models/ModelAttachmentsDingoOmni.js
+++ components/three-models/ModelAttachmentsDingoOmni.js
@@ -6,41 +6,30 @@ import ModelAttachmentHokuyo from "./ModelAttachmentHokuyo";
 import ModelAttachmentMicrostrain15 from "./ModelAttachmentMicrostrain15";
 
 // position data
-import attachmentPositionData from "/public/json/AttachmentPositionDingoOmni";
+import dataFile from "/public/json/DataDingoOmni";
+const attachmentPositionData = dataFile.attachmentPositions;
 import attachmentPositionHeights from "/public/json/AttachmentPositionHeights";
 
 export default function Model(props) {
-  /*
-  // the way I was defining the array of attachment models. The new method is using a dictionary.
-
   const attachmentPosition = attachmentPositionData[props.modelAttachmentPosition];
-  const attachmentModels = [
-    <></>,
-    <ModelAttachmentVelodyne key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]}/>, 
-    <group key="null">
-      <ModelAttachmentPlate50 key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />
-      <ModelAttachmentVelodyne key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[1]}/>
-    </group>,
-    <group key="null">
-      <ModelAttachmentPlate100 key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />
-      <ModelAttachmentVelodyne key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[2]}/>
-    </group>,
-    <ModelAttachmentHokuyo key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />
-    <ModelAttachmentMicrostrain15 key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />
-  ];
-  */
-
-  const attachmentPosition = attachmentPositionData[props.modelAttachmentPosition];
-  var attachmentModels={ 
-    none: <></>, 
-    velodyne_low:<ModelAttachmentVelodyne key="velodyne_low" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]}/>, 
-    velodyne_mid:<group key="null"><ModelAttachmentPlate50 key="velodyne_mid" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} /><ModelAttachmentVelodyne key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[1]}/></group>,
-    velodyne_high:<group key="null"><ModelAttachmentPlate100 key="velodyne_high" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} /><ModelAttachmentVelodyne key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[2]}/></group>,
-    hokuyo:<ModelAttachmentHokuyo key="hokuyo" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />,
-    microstrain:<ModelAttachmentMicrostrain15 key="microstrain" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />
+  var attachmentModels = {
+    none: <></>,
+    velodyne_low: <ModelAttachmentVelodyne key="velodyne_low" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />,
+    velodyne_mid: (
+      <group key="null">
+        <ModelAttachmentPlate50 key="velodyne_mid" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />
+        <ModelAttachmentVelodyne key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[1]} />
+      </group>
+    ),
+    velodyne_high: (
+      <group key="null">
+        <ModelAttachmentPlate100 key="velodyne_high" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />
+        <ModelAttachmentVelodyne key="null" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[2]} />
+      </group>
+    ),
+    hokuyo: <ModelAttachmentHokuyo key="hokuyo" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />,
+    microstrain: <ModelAttachmentMicrostrain15 key="microstrain" dataOne={attachmentPosition} dataTwo={attachmentPositionHeights[0]} />,
   };
 
-  return (
-    attachmentModels[props.userSelectedData.value]
-  );
+  return attachmentModels[props.userSelectedData.value];
 }
